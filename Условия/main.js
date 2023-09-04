1d42c42011586eec063355549322369704019960
Andreitacion <131300124+Andreitacion@users.noreply.github.com>
2023-09-04 11:01:14 +0300

    Add files via upload

---
+++ Условия/main.js
@@ -0,0 +1,15 @@
+let FirstName ='Andrew'
+let LastName = 'Antonovich'
+let age = '18'
+let country = 'Russia'
+let sity = 'Saint Petersburg'
+
+const Andrew = '18'
+
+if (Andrew === '18') {
+    console.log('Андрей совершеннолетний')
+} else if (Andrew === '17') {
+    console.log('Андрей несовершеннолетний')
+}
+
+
